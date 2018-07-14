7928e35d938d02f0b4d25941cce9ece195d11fe0
Pryanishnikovsa <Pryanishnikovsa@gmail.com>
2018-07-14 14:34:56 +0300

    dwq

--- DistributionElements/src/Game.java
+++ DistributionElements/src/Game.java
@@ -2,65 +2,67 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.text.JTextComponent;
 public class Game extends JFrame {
+
 
     JButton button[] = new JButton[67];
     JFrame fr = new JFrame();
     ImageIcon icon = new ImageIcon("C:\\Users\\USER\\Desktop/1529760734308.jpg");
     JPanel panel = new JPanel();
+    int b;
 
     public class TestActionListener implements ActionListener{
         public void actionPerformed(ActionEvent e){//Тута происходят все действия при нажатии на кнопку
-            //System.out.println("123");
-            //System.out.println(e.getSource().GetName());
+            Object com;
 
-            //Object com;
-            //String str;
-
-            //com = (JButton) e.getSource();
-            //System.out.println(com.GetName());
-            //tr = ((JTextComponent) com).getText();
-            //System.out.print(((JTextComponent) com).getText()+"__");
-
+            com = e.getSource();
+            System.out.println(((JButton) com).getName());
         }
     }
 
-    public void CreateBoard(){
+    public void CreateBoard(int Length, int Width){
         fr.add(panel);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel.setLayout(new GridLayout(9,8));
+        panel.setLayout(new GridLayout(Length + 1,Width));
         fr.setBounds(300,0,700,700);
         fr.setVisible(true);
         ActionListener actionListener = new TestActionListener();
 
-        for(int i = 1;i<65;i++) {
-           button[i] = new JButton(icon);
-           button[i].setIcon(icon);
-           button[i].setName(Integer.toString(i));
-           fr.getContentPane().add(button[i]);
-           button[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    for (int i = 1; i < 65; i++) {
-                        button[i].addActionListener(actionListener);
+        b = 0;
+        for(int i = 1;i<=Length;i++) {
+            for(int c = 1;c<=Width;c++) {
+
+               b = i + c;
+                button[b] = new JButton(icon);
+                button[b].setIcon(icon);
+                button[b].setName(Integer.toString(i) + Integer.toString(c));
+                fr.getContentPane().add(button[b]);
+                button[b].addActionListener(new ActionListener() {
+               //     @Override
+                    public void actionPerformed(ActionEvent e) {
+                        int x;
+
+
+                        for (x = 1; x < 65; x++) {
+                            button[x].addActionListener(actionListener);
+                        }
                     }
-                }
+                });
+                panel.add(button[b]);
             }
-            );
-            panel.add(button[i]);
+
         }
 
-        JButton cancelbutton = new JButton("Выход");
-        cancelbutton.addActionListener(new ActionListener() {//Слушатель для кнопки выход
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fr.dispose();//Закрывает frame
-            }
-        });
-        button[66] = new JButton("Перезагрузка");
-        panel.add(cancelbutton);
-        panel.add(button[66]);
+        //JButton cancelbutton = new JButton("Выход");
+        //cancelbutton.addActionListener(new ActionListener() {//Слушатель для кнопки выход
+        //    @Override
+        //    public void actionPerformed(ActionEvent e) {
+        //        fr.dispose();//Закрывает frame
+        //    }
+        //});
+        //button[66] = new JButton("Перезагрузка");
+        //fr.add(cancelbutton);
+        //fr.add(button[66]);
 
         fr.setVisible(true);
     }
@@ -68,14 +70,14 @@ public class Game extends JFrame {
 
 
     public static void main(String[] args){
-        Game game = new Game();
-        game.CreateBoard();
-
         int[][] ResultTable; // Двумерный массив с результатом
         int Length, Width; // Переменные длина и ширина поля
         Length = 8;
         Width = 8;
-        //DistributeElements distributeElements = new DistributeElements();
+
+        Game game = new Game();
+        game.CreateBoard(Length, Width);
+
         ResultTable = DistributeElements.CreateDistribution(Length,Width);
         int a,b; // Счетчики циклов
 
